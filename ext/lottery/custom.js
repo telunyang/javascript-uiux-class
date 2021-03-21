3e7a2eeb9fe5249bdbb490cd7dd230d970fdf645
DarrenYang <telunyang@gmail.com>
2021-03-22 00:27:14 +0800

    更新威力彩程式碼，簡化並買增加說明

--- ext/lottery/custom.js
+++ ext/lottery/custom.js
@@ -1,41 +1,76 @@
-//宣告我們剛剛寫好的取亂數的function
+/**
+ * 威力彩 遊戲介紹
+ * https://www.taiwanlottery.com.tw/Superlotto638/index.asp
+ */
+
+//取得亂數
 function getRandom(x) {
     /**
-     * 假設 x 為 10，產生的值就落在 0 - 9 之間，
+     * 假設 x 為 38，產生的值就落在 0 - 37 之間，
      * 此時程式後面再加 1，
-     * 代表產生的值落在 1 - 10 之間，
+     * 代表產生的值落在 1 - 38 之間，
      * 再進行回傳
      */
     return Math.floor(Math.random() * x) + 1;
 }
 
-//再來寫產生威力彩號碼的function
+//放置第一區 6 個號碼的陣列變數
+let arr = [];
+
+//放置第二區 1 個號碼的數值變數
+let n = 0;
+
+//產生威力彩號碼
 function getPowerNum() {
-    //首先我們先宣告一個字串，用來裝要回傳的結果
-    let status = "第一區號碼為：";
-    
-    //宣告一個變數用來裝隨機產生的數字
-    let n = 0;
+    //一注威力彩號碼有 6 個所以我們讓迴圈跑六次
+    for (i = 1; i <= 6; i++) {
+        /** 
+         * 用 indexOf 判斷該數字之前有沒有出現過，
+         * 如果有，就將 i 遞減，等於讓這輪重跑一次。
+         */
 
-    //一注威力彩號碼有6個所以我們讓迴圈跑六次
-    for (i = 0; i <= 5; i++) {
-        //用indexOf判斷該數字之前有沒有出現過
-        n = getRandom(38);
+        //隨機取得 1 ~ 38 之間的數字
+        let num = getRandom(38);
 
-        //如果有出現過就重跑一次迴圈
-        if (status.indexOf(n) > 0) {
-            //將 i 遞減，等於這輪重跑一次
-            i -= 1; //等同於 i = i - 1 或 i--
+        //檢查是否有出現過(重複)
+        if (arr.indexOf(num) > -1) {
+            i--;
             continue;
         } else {
-            //沒出現過的話就寫進字串裡
-            status += n + " ";
+            //沒出現過的話就寫進陣列裡
+            arr.push(num);
         }
     }
 
+    /**
+     * 排序陣列當中的值，由小至大
+     * 
+     * 參考網頁:
+     * https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+     */
+    arr.sort(function(a, b) {
+        /**
+         * 用數字排序
+         * a - b 指的是由小到大
+         * b - a 指的是由大到小
+         * 
+         * 若只有 arr.sort()，括號裡面沒有排序用的 function，
+         * 則以字串作為排序依據，例如 [20, 5, 9, 10]，
+         * 就會變成 [10, 20, 5, 9]
+         */
+        return a - b;
+    });
+
+    //第二區號碼
+    n = getRandom(8);
+
     //因為威力彩有分兩區，第二區為 01 ~ 08 隨機一數字，所以我們在回傳時加上。
-    return status + "第二區號碼為：" + getRandom(8);
+    return `第一區號碼為： ${arr.join(",")}，第二區號碼為： ${n}`;
 }
 
+/**
+ * 回傳結果範例
+ *   第一區號碼為： 8,9,22,25,35,37，第二區號碼為： 7
+ *   第一區號碼為： 3,14,17,18,31,36，第二區號碼為： 4
+ */
 alert(getPowerNum());
-//回傳結果："A組號碼為：19 11 2 30 20 8 B組數字為：2"
